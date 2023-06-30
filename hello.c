81a927fb03f0c9b04dd0507d1d0fc849ac07081c
Nicolas Fuentes <nfuentes11@gmail.com>
2023-06-29 21:18:23 -0400

    CS50 week 2

--- hello.c
+++ hello.c
@@ -1,5 +1,6 @@
-
-int main(void)
-{
-    printf("Hello,world\n");
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
 }
